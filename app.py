6de1e8fd0992a744020dbb1e4d286ce3a2a2e5bb
ian <ianeisenberg90@gmail.com>
2019-07-23 17:28:11 -0700

    added more to plot

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -9,7 +10,8 @@ import seaborn as sns
 import textwrap
 
 # helper functions
-def get_display_title(txt, max_length=30):
+def get_display_title(entry, max_length=30):
+    txt = entry['title'] + ': %s refs' % len(entry['dates'])
     wrapped = textwrap.wrap(txt, max_length, break_long_words=False)
     return '\n'.join(wrapped)
 
@@ -18,7 +20,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-focus_file', default=None)
 parser.add_argument('-relative', action='store_true')
 args = parser.parse_args()
-
 meta = json.load(open('dissertation_refs/meta.json','r'))
 relative_date = args.relative
 
@@ -37,13 +38,13 @@ for i,entry in enumerate(meta):
         print('Something went wrong, no dates found for %s' % entry['file'])
         to_delete.append(i)
     else:
-        
+        pub_date = int(entry['date'][-4:])
         if relative_date:
-            pub_date = int(entry['date'][-4:])
             f = lambda x: int(x)-pub_date
             entry['dates'] = list(map(f, dates))
         else:
             entry['dates'] = list(map(int, dates))
+        entry['dates'] = [d for d in entry['dates'] if d <= pub_date]
     if entry['file'] == args.focus_file:
         focused = i
 
@@ -73,50 +74,68 @@ for entry in meta:
 df = pd.DataFrame(meta)
 df.loc[:,'cite_count'] = df.dates.apply(len)
 
+# change area names
+area_conversion = {name:name.upper()[0] for name in df.area.unique()}
+df.area.replace(area_conversion, inplace=True)
+
 # plot
-colors = ['r','b']
+colors = [np.array([230,159,0])/255,
+          np.array([86,180,233])/255,
+          np.array([0,114,178])/255]
 sns.set_context('poster')
 size=15
 f = plt.figure(figsize=(size, size*.5))
+diff_ax = f.add_axes([0,.1,.4,.8])
 context_ax = f.add_axes([0,.1,.4,.4])
-current_ax = f.add_axes([0,.55,.4,.4])
-cum_ax = f.add_axes([.425,.1,.3,.85])
+current_ax = f.add_axes([0,.5,.4,.4])
+cum_ax = f.add_axes([.425,.1,.28,.85])
 num_ax = f.add_axes([.75,.1,.25,.3])
 misc_ax = f.add_axes([.8,.55,.2,.4])
 
+
 # plot date distributions
-sns.distplot(meta[focused]['dates'], ax=current_ax, bins=20, 
-             hist_kws={'rwidth':.9}, color=colors[0])
-sns.distplot(all_dates, ax=context_ax, bins=20, 
-             hist_kws={'rwidth':.9}, color=colors[1])
+min_date = math.floor(np.min(all_dates)/5)*5
+max_date = math.ceil(np.max(all_dates)/5)*5
+bins = range(min_date, max_date, 5)
+
+current_bins = current_ax.hist(meta[focused]['dates'], bins=bins, color=colors[0], rwidth=.9)
+context_bins = context_ax.hist(all_dates, bins=bins, color=colors[1], rwidth=.9)
+
+# get normalized difference and plot
+normed_diff = current_bins[0]/sum(current_bins[0]) - context_bins[0]/sum(context_bins[0])
+diff_ax.bar(current_bins[1][:-1]+2.5,normed_diff*-1, width=4.3, alpha=.2, color='k')
+diff_ax.set_ylim([-np.max(abs(normed_diff)), np.max(abs(normed_diff))])
+
 # plot number of citationd distribution
 num_cites = [len(x['dates']) for x in meta]
-sns.distplot(num_cites, ax=num_ax, kde=False,
-             hist_kws={'rwidth':.9})
-sns.boxplot('area', 'cite_count', data=df, ax=misc_ax)
+num_ax.hist(num_cites, rwidth=.9, color=colors[2], bins=len(num_cites)//5)
+sns.boxplot('area', 'cite_count', data=df, ax=misc_ax, palette=sns.hls_palette())
 
 # plot date averages
-sns.rugplot(averages, ax=context_ax, color='k')
+sns.rugplot(averages, ax=cum_ax, color='k')
 # plot CDFs
 for i, cdf in enumerate(cdfs):
     if i == focused:
         cum_ax.plot(date_bins[:-1], cdf, color=colors[0], zorder=10)
     else:
-        cum_ax.plot(date_bins[:-1], cdf, color=colors[1], alpha=.3)
+        cum_ax.plot(date_bins[:-1], cdf, color=colors[1], alpha=.2)
     
 
 # set up axes
+current_ax.patch.set_facecolor('None')
+context_ax.patch.set_facecolor('None')
+
 current_ax.tick_params(bottom=False, labelbottom=False,
                        labelleft=False, left=False)
 context_ax.tick_params(labelleft=False, left=False, labelsize=size*1.5)
 cum_ax.tick_params(labelleft=False, left=False, labelsize=size*1.5)
 num_ax.tick_params(labelleft=False, left=False, labelsize=size*1.5)
-misc_ax.tick_params(bottom=False, labelbottom=False, 
-                    labelsize=size*1.5,
+misc_ax.tick_params(labelsize=size*1.5,
                     length=size/2, width=1)
 context_ax.invert_yaxis()
 for ax in f.get_axes():
     sns.despine(ax=ax, offset=size, left=True)
+sns.despine(ax=current_ax, bottom=True, left=True)
 
 # set xaxis
 x_min = min(current_ax.get_xlim()[0], context_ax.get_xlim()[0])
@@ -138,12 +157,12 @@ num_ax.set_xlabel('# of Citations', fontsize=size*2)
 misc_ax.set_ylabel('# of Citations', fontsize=size*2)
 
 # add legends
-current_ax.text(.2,.8, 'Focused Dissertation', fontsize=size*1.5,
+current_ax.text(.1,.8, 'Focused Dissertation', fontsize=size*1.5,
                 transform=current_ax.transAxes, color=colors[0])
-current_ax.text(.2,.7, get_display_title(meta[focused]['title']), 
+current_ax.text(.1,.7, get_display_title(meta[focused]), 
                 fontsize=size*1.25,
                 transform=current_ax.transAxes, color=colors[0], va='top')
-context_ax.text(.2,.2, 'All Dissertations', fontsize=size*1.5,
+context_ax.text(.1,.2, 'All Dissertations', fontsize=size*1.5,
                 transform=context_ax.transAxes, color=colors[1])
 cum_ax.text(.2,.8, 'Date CDFs', fontsize=size*1.5,
                 transform=cum_ax.transAxes, color='k')
